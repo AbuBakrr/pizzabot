f2f1c4484447e67fae55b80b26463d7fcb47b75d
Abu-Bakr <abubakr.jabbarov@gmail.com>
2021-12-21 21:24:17 +0500

    Added measuring method to Instruct class

--- InstructorTests/InstructorTests.swift
+++ InstructorTests/InstructorTests.swift
@@ -6,11 +6,37 @@
 //
 
 import XCTest
+import Instructor
+
+final class Instructor {
+    
+    func measureDistance(from firstLocation: Location, to secondLocation: Location) -> Int {
+        return abs(firstLocation.x - secondLocation.x) + abs(firstLocation.y - secondLocation.y)
+    }
+}
 
 class InstructorTests: XCTestCase {
 
+    var sut: Instructor!
+    
+    override func setUp() {
+        super.setUp()
+        sut = Instructor()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
     func test_calculatesCorrectDistances() {
+        // Given
+        let locationA = Location(x: 1, y: 3)
+        let locationB = Location(x: 4, y: 1)
         
+        // Then
+        let distance = sut.measureDistance(from: locationA, to: locationB)
+        XCTAssertEqual(distance, 5)
     }
 
 }
