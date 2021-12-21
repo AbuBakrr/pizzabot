6675d8028cae1569d78c40177ec353d82ae9806f
Abu-Bakr Jabbarov <Abu-Bakr@Abu-Bakrs-MacBook-Pro.local>
2021-12-21 17:04:24 +0500

    Instructor tests init

---
+++ PizzabotTests/InstructorTests.swift
@@ -0,0 +1,16 @@
+//
+//  InstructionGeneratorTests.swift
+//  PizzabotTests
+//
+//  Created by Abu-Bakr Jabbarov on 12/21/21.
+//
+
+import Foundation
+import XCTest
+
+class Instructor: XCTestCase {
+    
+    func test() {
+        
+    }
+}
