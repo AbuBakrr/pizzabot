e051827c62f9c8c91fb42c3c1da31ee88025444b
Abu-Bakr <abubakr.jabbarov@gmail.com>
2021-12-21 21:46:20 +0500

    Added some more locations to test

--- InstructorTests/DistanceCalculatorTests.swift
+++ InstructorTests/DistanceCalculatorTests.swift
@@ -19,12 +19,31 @@ class DistanceCalculatorTests: XCTestCase {
 
     func test_calculatesCorrectDistances() {
         // Given
-        let locationA = Location(x: 1, y: 3)
-        let locationB = Location(x: 4, y: 1)
+        let routes = makeRouteItems()
         
         // Then
-        let distance = DistanceCalculator.measureDistance(from: locationA, to: locationB)
-        XCTAssertEqual(distance, 5)
+        for route in routes {
+            let distance = DistanceCalculator.measureDistance(from: route.start, to: route.end)
+            XCTAssertEqual(distance, route.distance)
+        }
     }
 
+    // MARK: - Helpers
+    
+    struct Route {
+        let start: Location
+        let end: Location
+        let distance: Int
+    }
+    
+    private func makeRouteItems() -> [Route] {
+        return [
+            Route(start: Location(x: 0, y: 0), end: Location(x: 0, y: 0), distance: 0),
+            Route(start: Location(x: 0, y: 0), end: Location(x: 1, y: 3), distance: 4),
+            Route(start: Location(x: 1, y: 3), end: Location(x: 0, y: 0), distance: 4),
+            Route(start: Location(x: 1, y: 4), end: Location(x: 1, y: 1), distance: 3),
+            Route(start: Location(x: 4, y: 2), end: Location(x: 2, y: 3), distance: 3),
+        ]
+    }
 }
+
