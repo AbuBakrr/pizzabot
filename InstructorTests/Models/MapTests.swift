d8c7fb447c6e2e9999e6acffa4becbd5704ae6cd
Abu-Bakr <abubakr.jabbarov@gmail.com>
2021-12-25 23:26:48 +0500

    MapTests refactored

--- InstructorTests/Models/MapTests.swift
+++ InstructorTests/Models/MapTests.swift
@@ -9,35 +9,26 @@ import XCTest
 import Instructor
 
 class MapTests: XCTestCase {
-
-    var sut: Map!
-    
-    override func setUp() {
-        super.setUp()
-        sut = Map(width: 5, height: 5)
-    }
-    
-    override func tearDown() {
-        sut = nil
-        super.tearDown()
-    }
     
     func test_locationInclusion() {
+        // Given
+        let map = Map(width: 5, height: 5)
+        
         // Case 1
         let location1 = Location(x: 0, y: 0)
-        XCTAssertEqual(sut.includes(location: location1), true)
+        XCTAssertEqual(map.includes(location: location1), true)
         
         // Case 2
         let location2 = Location(x: 6, y: 5)
-        XCTAssertEqual(sut.includes(location: location2), false)
+        XCTAssertEqual(map.includes(location: location2), false)
         
         // Case 3
         let location3 = Location(x: 5, y: 5)
-        XCTAssertEqual(sut.includes(location: location3), true)
+        XCTAssertEqual(map.includes(location: location3), true)
         
         // Case 4
         let location4 = Location(x: 0, y: 6)
-        XCTAssertEqual(sut.includes(location: location4), false)
+        XCTAssertEqual(map.includes(location: location4), false)
     }
 }
 
